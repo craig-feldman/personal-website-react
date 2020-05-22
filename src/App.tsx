d1d70c471e0caec54e15ed7c0efb9e2dbf3f00b4
Craig Feldman <craig.feldy@gmail.com>
2020-05-22 23:06:43 +0200

    Removes unused banner and makes banner use current year

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,12 @@ import "./App.css";
 import Terminal from "./components/Terminal";
 
 const getYear = () => {
-  return 2020;
+  return new Date().getFullYear();
 };
 
 const welcomeMessage = `Welcome to my site fellow humans and bots.
 Type 'help' to view a list of available commands.
 `;
-
-const banner =
-  "                _          __     _     _                       \n" +
-  "               (_)        / _|   | |   | |                      \n" +
-  "  ___ _ __ __ _ _  __ _  | |_ ___| | __| |_ __ ___   __ _ _ __  \n" +
-  " / __| '__/ _` | |/ _` | |  _/ _ \\ |/ _` | '_ ` _ \\ / _` | '_ \\ \n" +
-  "| (__| | | (_| | | (_| | | ||  __/ | (_| | | | | | | (_| | | | |\n" +
-  " \\___|_|  \\__,_|_|\\__, | |_| \\___|_|\\__,_|_| |_| |_|\\__,_|_| |_|\n" +
-  "                   __/ |                                        \n" +
-  "     \u00A9 " +
-  getYear() +
-  "       |___/                                         ";
 
 const bannerCondensed =
   "             _         __     _    _                 \n" +
