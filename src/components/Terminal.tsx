de2d78bd133ac3c99e4e8d41d9e7754852abdb0a
Craig Feldman <craig.feldy@gmail.com>
2020-05-23 15:51:10 +0200

    Adds support for autocomplete on mobile (using period key).

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -564,8 +564,19 @@ type InputAreaProps = {
 const InputArea = (props: InputAreaProps) => {
   const [input, setInput] = useState("");
 
+  /**
+   * Sets the input state to the value of the input.
+   *
+   * If the input is a period, we get the autocomplete for the input up to, but excluding the period.
+   * We handle this case specially to allow autocomplete on mobile because KeyboardEvent.key tends to be 'unidentified' for inputs on mobile keyboards.
+   */
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value);
+    const inputValue = event.target.value;
+    if (inputValue.charAt(inputValue.length - 1) === ".") {
+      setInput(
+        props.getAutocomplete(inputValue.substring(0, inputValue.length - 1))
+      );
+    } else setInput(inputValue);
   };
 
   const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -583,6 +594,7 @@ const InputArea = (props: InputAreaProps) => {
         setInput(props.getHistory("down"));
         break;
       case "Tab":
+        // Provide autocomplete on tab. For mobile, we have to handle autocomplete in the input's onChange event.
         event.preventDefault();
         console.log("Tab pressed");
         setInput(props.getAutocomplete(input));
