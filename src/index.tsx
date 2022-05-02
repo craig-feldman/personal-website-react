5346e83ecf4bea77adb4bf1b4088febd9be279b0
Craig Feldman <craig.feldy@gmail.com>
2022-05-02 23:26:08 +0200

    Update to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
